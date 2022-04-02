42128c41e38cd75a8bf9fc0443f07d14121c4342
Documenter.jl <documenter@juliadocs.github.io>
2022-04-02 15:50:59 +0000

    build based on 6f175c7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RiccatiRecursion","category":"page"},{"location":"#RiccatiRecursion","page":"Home","title":"RiccatiRecursion","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RiccatiRecursion.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RiccatiRecursion]","category":"page"}]
+}
